dcafcf6a5e8aec8f5731c52ce2660642c8d580a4
rubenantareinc <rubenarenaapplication@gmail.com>
2025-07-05 02:49:00 +0300

    Create Navbar.jsx

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <nav style={{
+      background: '#e74c3c', color: '#fff',
+      padding: '1rem', textAlign: 'center',
+      fontSize: '1.5rem', fontWeight: 'bold'
+    }}>
+      🌟 Emome 🌟
+    </nav>
+  );
+}
